a9be4c1edc2dadff04c1c4b5924ba334019d7d2a
CBrazell23 <31522900+CBrazell23@users.noreply.github.com>
2020-05-27 21:25:00 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
  * Recursively computes Fibonacci numbers.
  * CSC 225, Assignment 6
@@ -5,6 +7,20 @@
  * TODO: Complete this file.
  */
 
-int main(void) {
+int fib(int x){
+   if(x < 0){
+      return 0;
+   }
+   if(x == 1){
+      return x;
+   }
+   return fib(x - 1) + fib(x - 2);
+}
 
+int main(void) {
+   int x;
+   printf("Enter an integer: ");
+   scanf("%d", &x);
+   printf("f(%d) = %d\n", x, fib(x));
+   return 0;
 }
