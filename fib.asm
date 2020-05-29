0ef18948d9927ef3c1be112948af2e7e91f52ce8
CBrazell23 <31522900+CBrazell23@users.noreply.github.com>
2020-05-28 21:18:36 -0700

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -6,50 +6,68 @@
 
 ; int fib(int)
 ; TODO: Implement this function.
-FIBFN ST R0, SAVER0
-      ST R1, SAVER1
-      ST R2, SAVER2
-      ST R3, SAVER3
-      ST R4, SAVER4
-      ST R5, SAVER5
-      ST R6, SAVER5
-      ST R7, SAVER7
-      
-      ADD R0, R2, #0
-      BRnz NEG
-      
-      ADD R0, R0, #-1
-      BRz BASE
-      
-      ADD R3, R2, #0
-      ADD R2, R2, #-2
-      LEA R4, FIBFN
-      
-      ADD R2, R3, #0
-      ADD R2, R2, #-1
-      LEA R5, FIBFN
-      
-      ADD R3, R4, R5
-      LDR R5, R3, #3
-      
-FIN   LD R0, SAVER0
-      LD R1, SAVER1
-      LD R2, SAVER2
-      LD R3, SAVER3
-      LD R4, SAVER4
-      LD R5, SAVER5
-      LD R6, SAVER6
-      LD R7, SAVER7
-      
-      HALT
-      
-NEG   AND R3, R3, #0
-      LDR R5, R3, #3
-      BRnzp FIN
+FIBFN 
 
-BASE  ADD R3, R3, #0
-      LDR R5, R3, #3
-      BRnzp FIN
+SUMFN   ADD R6, R6, #-1 ; Push space for ret. val.
+        ADD R6, R6, #-1 ; Push the return address.
+        STR R7, R6, #0
+        ADD R6, R6, #-1 ; Push the dynamic link.
+        STR R5, R6, #0
+        ADD R5, R6, #-1 ; Set the frame pointer.
+
+        ADD R6, R6, #-2
+        LDR R0, R5, #4
+        ADD R1, R0, #0
+
+        ADD R0, R0, #0
+        BRnz NEG
+      
+        ADD R0, R0, #-1
+        BRz BASE
+        
+        ADD R6, R6, #-1
+        STR R0, R6, #0
+        
+        JSR FIBFN
+        LDR R2, R6, #0
+        ADD R6, R6, #2
+        
+        STR R2, R5, #0
+        
+        ADD R0, R0, #-1
+        ADD R6, R6, #-1
+        STR R0, R6, #0
+        
+        JSR FIBFN
+        LDR, R4, R6, #0
+        ADD R6, R6, #2
+        
+        STR R4, R5, #0
+        
+        LDR R0, R5, #0
+        LDR R1, R5, #0
+        ADD R0, R0, R1
+        
+        STR R0, R5, #3
+        BRnzp FIN
+      
+NEG     AND R3, R3, #0
+        STR R3, R5, #3
+        BRnzp FIN
+
+BASE    ADD R3, R3, #1
+        STR R3, R5, #3
+        BRnzp FIN
+        
+FIN     ADD R5, R5, #2
+        LDR R5, R6, #0  ; Pop the dynamic link.
+        ADD R6, R6, #1
+        LDR R7, R6, #0  ; Pop the return address.
+        ADD R6, R6, #1
+        RET             ; Return.
+
+      
+
 
 PROMPT  .STRINGZ "Enter an integer: "
 SAVER0  .FILL x0000
