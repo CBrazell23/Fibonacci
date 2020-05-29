e7b6b1143406bcd3b0aaba0008e99f854a2a0ddc
CBrazell23 <31522900+CBrazell23@users.noreply.github.com>
2020-05-28 22:05:48 -0700

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -8,7 +8,7 @@
 ; TODO: Implement this function.
 FIBFN 
 
-SUMFN   ADD R6, R6, #-1 ; Push space for ret. val.
+        ADD R6, R6, #-1 ; Push space for ret. val.
         ADD R6, R6, #-1 ; Push the return address.
         STR R7, R6, #0
         ADD R6, R6, #-1 ; Push the dynamic link.
@@ -17,34 +17,38 @@ SUMFN   ADD R6, R6, #-1 ; Push space for ret. val.
 
         ADD R6, R6, #-2
         LDR R0, R5, #4
-        ADD R1, R0, #0
+        LDR R1, R5, #4
 
         ADD R0, R0, #0
         BRnz NEG
       
         ADD R0, R0, #-1
         BRz BASE
+        ADD R0, R0, #1
         
         ADD R6, R6, #-1
-        STR R0, R6, #0
+        
+        ADD R1, R1, #-1
+        STR R1, R6, #0
         
         JSR FIBFN
         LDR R2, R6, #0
         ADD R6, R6, #2
         
         STR R2, R5, #0
+        LDR R3, R5, #4
+        ADD R3, R3, #-2
         
-        ADD R0, R0, #-1
         ADD R6, R6, #-1
-        STR R0, R6, #0
+        STR R3, R6, #0
         
         JSR FIBFN
         LDR, R4, R6, #0
         ADD R6, R6, #2
         
-        STR R4, R5, #0
+        STR R4, R5, #-1
         
-        LDR R0, R5, #0
+        LDR R0, R5, #-1
         LDR R1, R5, #0
         ADD R0, R0, R1
         
@@ -55,11 +59,12 @@ NEG     AND R3, R3, #0
         STR R3, R5, #3
         BRnzp FIN
 
-BASE    ADD R3, R3, #1
+BASE    AND R3, R3, #0
+        ADD R3, R3, #1
         STR R3, R5, #3
         BRnzp FIN
         
-FIN     ADD R5, R5, #2
+FIN     ADD R6, R6, #2
         LDR R5, R6, #0  ; Pop the dynamic link.
         ADD R6, R6, #1
         LDR R7, R6, #0  ; Pop the return address.
